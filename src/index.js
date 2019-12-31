2ad78b809bf1fb1ba412218350271f7ce865cf17
ashwinKumar0505 <ashwin.gps@gmail.com>
2019-12-31 15:05:39 +0530

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -63,6 +63,7 @@ const MenuBar = ({
       className={`menu ${className}`}
       ref={menubarRef}
       onClick={closeItemsHandler}
+      style={{ display: 'inline-flex' }}
     >
       <HamburgerMenu
         showItemsHandler={showItemsHandler}
